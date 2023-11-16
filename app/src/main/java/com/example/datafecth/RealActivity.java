7d315a432112f964ea0f28f82db120eb6ef07e00
revolve 810 <joycemutati72@gmail.com>
2023-11-16 19:46:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/datafecth/RealActivity.java
@@ -0,0 +1,14 @@
+package com.example.datafecth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real);
+    }
+}
